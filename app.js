85c14f40d0d53f64ed557125b347f41781dbeaa2
apachqq <work.tsyganov@mail.ru>
2022-04-03 23:37:17 +0300

    'methods_addNewNote_and_toUpperCase'

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const App = {
             inputValue: '',
             notes: []
         }
+    },
+    methods: {
+        addNewNote() {
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
     }
 }
 
