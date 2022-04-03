375bda391b69dc043fd527ed2782e18489ce8b1f
apachqq <work.tsyganov@mail.ru>
2022-04-03 23:35:27 +0300

    'data_elements_js'

---
+++ app.js
@@ -0,0 +1,12 @@
+const App = {
+    data() {
+        return {
+            title: 'Список заметок',
+            placeholderString: 'Введите название заметки',
+            inputValue: '',
+            notes: []
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
